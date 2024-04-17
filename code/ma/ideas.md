de0c1e0d3da90ff8df23bfd2c06c0501ecc40262
sebstec <sebstec@vikingtec.ninja>
2024-04-17 10:00:18 +0200

    modsecurity request blocking, logging, serverside request reflection

--- code/ma/ideas.md
+++ code/ma/ideas.md
@@ -3,3 +3,7 @@
 # fix
 - content type
 - allowed methods
+# config notes
+- in modsecurity.conf SecRuleEngine On
+# useful
+- read modsec log in container `watch -c -n 1 tail -n 32 modsec_audit.log`
--- code/ma/nodeserver/server.js
+++ code/ma/nodeserver/server.js
@@ -21,6 +21,25 @@ const server = createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "application/json" });
     res.write(JSON.stringify({ now: new Date() }));
     res.end();
+  } else if (req.url.includes("/reflect")) {
+    logger(req, res, i);
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk.toString(); // convert Buffer to string
+    });
+    req.on("end", () => {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.write(
+        JSON.stringify({
+          url: req.url,
+          method: req.method,
+          statusMessage: res.statusMessage,
+          statusCode: res.statusCode,
+          body: body,
+        }),
+      );
+      res.end();
+    });
   } else {
     logger(req, res, i);
     res.writeHead(200, { "Content-Type": "text/plain" });
