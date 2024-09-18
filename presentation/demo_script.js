f374278c76365f934491d9b2469ef6c19c9b2d58
sebstec <sebstec@vikingtec.ninja>
2024-09-18 12:11:15 +0200

    fix: added cleaning script for aurebesh feat: related work, proposal, evasion techniques basics

--- presentation/demo_script.js
+++ presentation/demo_script.js
@@ -1,11 +1,19 @@
 // INFO: Aurebesh.js
 
-const aurebesh_input = JSON.parse(
-    "{\"message\":\"A='',B=!A+A,C=!B+A,D=A+{},E=B[A++],F=B[G=A],H=++G+A,I=D[G+H],B[I+=D[A]+(B.C+D)[A]+C[H]+E+F+B[G]+I+E+D[A]+F][I]('p'+F+D[A]+'m'+'p'+E+'(A,++A)')()\"}",
-);
+// const cleared =
+//     'A=’’,B=!A+A,C=!B+A,D=A+{},E=B[A++],F=B[G=A],H=++G+A,I=D[G+H],B[I+=D[A]+(B.C+D)[A]+C[H]+E+F+B[G]+I+E+D[A]+F][I](’p’+F+D[A]+’m’+’p’+E+’(A,++A)’)()'.replace(
+//         /’/g,
+//         "'",
+//     );
+// console.log(`cleared message: ${cleared}`);
+
+const aurebesh_input = JSON.parse(`{\"message\":\"${cleared}\"}`);
 console.log(`aurebesh input JSON parsed: ${aurebesh_input.message}`);
 // aurebesh_input.message = decodeURIComponent(aurebesh_input.message);
 // aurebesh_input.message = aurebesh_input.message.normalize('NFKC');
 // console.log(`aurebesh input decoded: ${aurebesh_input}`)
 document.getElementById('div1').insertAdjacentHTML('afterbegin', `<img src=1 onerror=${aurebesh_input.message}>`);
 // document.getElementById('div1').insertAdjacentHTML('afterbegin', `${input.message}>`)
+
+// INFO: Function constructor
+
